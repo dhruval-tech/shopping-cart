fe6384e12041193c4c709c729b031c0ef1dbb966
dgandhi <dgandhi@splunk.com>
2023-12-02 20:30:42 +0530

    Configured Routes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# shopping-cart
+# A brief of this shopping-cart application
 A shopping cart using React, TypeScript, and Bootstrap
+
+# Used below npm packages
+- react-router-dom
+- bootstrap
+- react-bootstrap
--- shoppingcart/src/App.tsx
+++ shoppingcart/src/App.tsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/home"
+import { Store } from "./pages/store"
+import { About } from "./pages/about"
 
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+      <Container>
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+          <Route path="store" element={ <Store /> }/>
+          <Route path="/about" element={ <About /> }/>
+        </Routes>
+      </Container>
   )
 }
 
